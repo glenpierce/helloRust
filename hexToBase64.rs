66afcd28a85180a9147982dfb0879e91abd3dba4
Glen Pierce <glen.piercev@gmail.com>
2020-10-18 23:05:04 -0700

    varargs convert function stub created

--- hexToBase64.rs
+++ hexToBase64.rs
@@ -5,11 +5,22 @@ fn main() {
     if(args.len() > 1) {
         let mut i = args[1].len() - 1;
         let char_vec: Vec<char> = args[1].chars().collect();
-        while i > 0 {
-            println!("{:?}", char_vec[i]);
-            i -= 1;
+        while i > 1 {
+            convertToBase64(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
+            i -= 3;
+        }
+        if(i == 1) {
+            convertToBase64(&[char_vec[i], char_vec[i-1]]);
+        }
+        if(i == 0) {
+            convertToBase64(&[char_vec[i]]);
         }
         println!("{:?}", args[1]);
     }
-    // println!("{:?}", args);
+}
+
+fn convertToBase64(args: &[char]) {
+    for arg in args {
+        println!("{}", arg);
+    }
 }
