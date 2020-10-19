cda8370c45069588d35a3a6d44cd63fd3617ac66
Glen Pierce <glen.piercev@gmail.com>
2020-10-18 22:50:32 -0700

    iterate backwards through string

--- hexToBase64.rs
+++ hexToBase64.rs
@@ -2,5 +2,14 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    if(args.len() > 1) {
+        let mut i = args[1].len() - 1;
+        let char_vec: Vec<char> = args[1].chars().collect();
+        while i > 0 {
+            println!("{:?}", char_vec[i]);
+            i -= 1;
+        }
+        println!("{:?}", args[1]);
+    }
+    // println!("{:?}", args);
 }
