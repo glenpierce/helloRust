200304c52230778b87a31975ff8ffc6903e31cca
Glen Pierce <glen.piercev@gmail.com>
2020-10-19 22:24:16 -0700

    convert from hex to bytes seems to work

--- hexToBase64.rs
+++ hexToBase64.rs
@@ -5,9 +5,12 @@ fn main() {
     if(args.len() > 1) {
         let mut i = args[1].len() - 1;
         let char_vec: Vec<char> = args[1].chars().collect();
-        while i > 1 {
+        while i > 2 {
             convertToBase64(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
             i -= 3;
+        }
+        if(i == 2) {
+            convertToBase64(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
         }
         if(i == 1) {
             convertToBase64(&[char_vec[i], char_vec[i-1]]);
@@ -19,8 +22,15 @@ fn main() {
     }
 }
 
-fn convertToBase64(args: &[char]) {
+fn convertToBase64(args: &[char]) -> std::vec::Vec<u8> {
+    let mut bytes:Vec<u8> = Vec::new();
     for arg in args {
-        println!("{}", arg);
+        let byte = u8::from_str_radix(&arg.to_string()[0..1], 16);
+        match byte {
+            Ok(value) => bytes.push(value),
+            Err(error) => {}
+        }
     }
+    println!("{:?}", bytes);
+    return bytes;
 }
