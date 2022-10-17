8fc4d02a32d5a80727cd8677088f42ea317c3e88
glen <glen.piercev@gmail.com>
2022-10-16 17:08:21 -0700

    It Works!

--- hexToBase64/hexToBase64.rs
+++ hexToBase64/hexToBase64.rs
@@ -2,37 +2,44 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("args: {:?}", args);
     if args.len() > 1 {
         let mut i: usize = args[1].len() - 1;
         let char_vec: Vec<char> = args[1].chars().collect();
-        let mut byteVec: Vec<u8> = Vec::new();
+        let mut byteVec: Vec<usize> = Vec::new();
         while i > 2 {
-            let mut convertedValue: Vec<u8> = convertToBase64(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
+            let mut convertedValue: Vec<usize> = convertToBase10(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
             byteVec.append(&mut convertedValue);
             i -= 3;
         }
         if i == 2 {
-            let mut convertedValue: Vec<u8> = convertToBase64(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
+            let mut convertedValue: Vec<usize> = convertToBase10(&[char_vec[i], char_vec[i-1], char_vec[i-2]]);
             byteVec.append(&mut convertedValue);
         }
         if i == 1 {
-            let mut convertedValue: Vec<u8> = convertToBase64(&[char_vec[i], char_vec[i-1]]);
+            let mut convertedValue: Vec<usize> = convertToBase10(&[char_vec[i], char_vec[i-1]]);
             byteVec.append(&mut convertedValue);
         }
         if i == 0 {
-            let mut convertedValue: Vec<u8> = convertToBase64(&[char_vec[i]]);
+            let mut convertedValue: Vec<usize> = convertToBase10(&[char_vec[i]]);
             byteVec.append(&mut convertedValue);
         }
-        println!("args[1]: {:?}", args[1]);
-        println!("byteVec: {:?}", byteVec);
+
+        let mut base10Value: usize = 0;
+        let mut place: u32 = 0;
+        for position in byteVec {
+            let multiplier: usize = usize::pow(16, place);
+            base10Value += position * multiplier;
+            place += 1;
+        }
+
+        println!("{}", base10Value);
     }
 }
 
-fn convertToBase64(args: &[char]) -> std::vec::Vec<u8> {
-    let mut bytes:Vec<u8> = Vec::new();
+fn convertToBase10(args: &[char]) -> std::vec::Vec<usize> {
+    let mut bytes:Vec<usize> = Vec::new();
     for arg in args {
-        let byte = u8::from_str_radix(&arg.to_string()[0..1], 16);
+        let byte = usize::from_str_radix(&arg.to_string()[0..1], 16);
         match byte {
             Ok(value) => bytes.push(value),
             Err(error) => {
@@ -40,6 +47,5 @@ fn convertToBase64(args: &[char]) -> std::vec::Vec<u8> {
             }
         }
     }
-    println!("bytes: {:?}", bytes);
     return bytes;
 }
